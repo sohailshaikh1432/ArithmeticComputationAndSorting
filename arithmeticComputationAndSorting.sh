a97be5f2aef5a1b28bad403e9e8db9982971db75
sohailshaikh1432 <sohailshaikh1432.ss@gmail.com>
2020-08-06 02:19:17 +0530

    Store the result in a Dictionary for every computation

--- arithmeticComputationAndSorting.sh
+++ arithmeticComputationAndSorting.sh
@@ -4,4 +4,13 @@ read -p "Enter input a : " a
 read -p "Enter input b : " b
 read -p "Enter input c : " c
 
-result=$(($a%$b+$c))
+declare -A operationDic
+operation1=$(($a+$b*$c))
+operation2=$(($a*$b+$c))
+operation3=$(($a+$b/$c))
+operation4=$(($a%$b+$c))
+
+for (( i=1 ; i<=4 ; i++ ))
+do
+	operationDic[$i]=$((operation$i))
+done
